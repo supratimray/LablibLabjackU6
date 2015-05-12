533c22c01aad03e5232f02ada85e080f62cc85c3
supratimray <supratimray@gmail.com>
2015-05-12 15:49:39 +0530

    Made several changes in the project settings and layout to maintain consistency with other Lablib plugins. Also created an empty LLDataDevice object. This version compiles and produces LablibLabjackU6.plugin in ~/Library/Application Support/Knot/Plugins. If Knot is run after the build, it recognises the data device as an "unnamed LLDataDevice"

---
+++ LLLabjackU6DataDevice.h
@@ -0,0 +1,20 @@
+//
+//  LLLabjackU6DataDevice.h
+//  LablibLabjackU6
+//
+//  Created by Supratim Ray on 12/05/15.
+//  Copyright (c) 2015 Supratim Ray. All rights reserved.
+//
+
+#ifndef LablibLabjackU6_LLLabjackU6DataDevice_h
+#define LablibLabjackU6_LLLabjackU6DataDevice_h
+
+# import <Lablib/Lablib.h>
+
+#endif
+
+@interface LLLabjackU6DataDevice : LLDataDevice {
+    
+}
+
+@end
---
+++ LLLabjackU6DataDevice.m
@@ -0,0 +1,18 @@
+//
+//  LLLabjackU6DataDevice.m
+//  LablibLabjackU6
+//
+//  Created by Supratim Ray on 12/05/15.
+//  Copyright (c) 2015 Supratim Ray. All rights reserved.
+//
+
+#import "LLLabjackU6DataDevice.h"
+
+@implementation LLLabjackU6DataDevice
+
++ (NSInteger)version;
+{
+    return kLLPluginVersion;
+}
+
+@end
